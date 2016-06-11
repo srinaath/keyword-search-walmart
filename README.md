3770fe0206e440073fe7d5d15b95f392caa50728
Srinaath Ravichandran <sravichandran@deloitte.com>
2016-06-11 17:04:33 -0600

    Project structure
    Initial commit with structure setup

---
+++ README.md
@@ -0,0 +1,9 @@
+## Walamart api search using hapi
+
+To install a version of hapi that works for this course, use the following syntax when installing:
+```sh
+npm install hapi@v8.8.1 --save
+```
+To run the application use
+nodemon server.js 
+```
---
+++ server.js
@@ -0,0 +1,54 @@
+const Hapi = require('hapi');
+const Path = require('path');
+const Hoek = require('hoek');
+
+
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, './')
+      }
+    }
+  }
+});
+server.register(require('inert'), (err) => {
+
+  if(err) {
+    throw err;
+  }
+});
+
+server.register(require('vision'), (err) => {
+
+  Hoek.assert(!err, err);
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'templates'
+  });
+});
+
+server.connection({ port: 3000 });
+
+
+server.ext('onRequest', function(request, reply) {
+  console.log('Request received' + request.path);
+  reply.continue();
+});
+
+server.ext('onPreResponse', function(request, reply) {
+  if(request.response.isBoom) {
+    return reply.view('error', request.response);
+  }
+  reply.continue();
+});
+
+
+server.route(require('./lib/routes'));
+
+server.start(function() {
+  console.log('Listening on ' + server.info.uri);
+});
